cc5a64c43141fd15579c323f5493b8b6edb08297
Romain Durand Saint Omer <romain.dso@gmail.com>
2018-05-01 21:29:53 +0200

    clean code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { FeatureFlags } from './FeatureFlags';
+import './index.css';
 
 // To load from anywhere (fetch, localStorage, a json file, Redux...)
 const flags = [
